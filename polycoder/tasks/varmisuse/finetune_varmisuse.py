fd3dd131d149c4d5d8d4fa7181d2e5d7f84a4c8e
Vy Vo <vy.vo@intel.com>
2023-09-29 12:10:24 -0700

    fixed error in loss function when no repair targets exist. also forced evaluation to happen with a Sequential version of the model to allow for other metrics to be computed, but haven't finished the eval code to print the metrics.

--- polycoder/tasks/varmisuse/finetune_varmisuse.py
+++ polycoder/tasks/varmisuse/finetune_varmisuse.py
@@ -1,10 +1,11 @@
 """VarMisuse task -- finetuning/evaluation."""
 
 import deepspeed
-
 from functools import partial
-from pathlib import Path
-from pprint import pformat
+import math
+import torch
+# from pathlib import Path
+# from pprint import pformat
 
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(
@@ -12,23 +13,18 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(
 
 from megatron.neox_arguments import NeoXArgs
 from megatron import mpu, print_rank_0
-from megatron.training import get_optimizer, get_learning_rate_scheduler
-from megatron.utils import get_total_params
+from megatron.training import get_optimizer, get_learning_rate_scheduler, evaluate
+from megatron.utils import get_total_params, reduce_losses
 
 from megatron.checkpointing import load_checkpoint
-# from megatron import get_tokenizer
-# from megatron.checkpointing import check_checkpoint_args, check_forward_pass
 from megatron.model.classification import Classification
-# from tasks.eval_utils import accuracy_func_provider
 from tasks.finetune_utils import finetune
+# from tasks.eval_utils import accuracy_func_provider
 # from megatron.arguments import core_transformer_config_from_args
 
-import torch
+from data_varmisuse import build_train_valid_test_data_iterators, get_batch_pipe
 
-
-# from megatron.utils import average_losses_across_data_parallel_group
 from sklearn.metrics import accuracy_score
-import datasets as trd
 
 
 # TODO: Run Docker test E2E
@@ -39,16 +35,17 @@ import datasets as trd
 
 def varmisuse_multi_class(neox_args, num_classes):
 
-    # """Build train and validation dataset."""
-    from data_varmisuse import build_train_valid_test_data_iterators
-
     def model_optim_lr_setup(neox_args, num_classes,
                              inference=False, get_key_value=None):
         """Build the model."""
         print_rank_0('building classification model for variable misuse task')
-        # TODO: fix the model for predicting pointers to parts of the sequence
-        model = Classification(neox_args, num_classes=num_classes,
-                               post_process=False, load_lm_checkpoint=neox_args.load)
+        model = Classification(neox_args, num_classes=num_classes, post_process=False,
+                               parallel_output=True, topology=mpu.get_topology(), get_key_value=True,
+                               loss_fn=compute_varmisuse_loss
+                               ) #, load_lm_checkpoint=neox_args.load)
+        if not neox_args.is_pipe_parallel:
+            # Export PipeParallel model to nn.Sequential model to avoid the overhead of deepspeed's pipe parallel training
+            model = model.to_sequential()
 
         optimizer, param_groups = get_optimizer(model=model, neox_args=neox_args)
         lr_scheduler = get_learning_rate_scheduler(optimizer=optimizer, neox_args=neox_args)
@@ -63,44 +60,43 @@ def varmisuse_multi_class(neox_args, num_classes):
         model.total_params = get_total_params(model)
         print_rank_0(f' > total params: {"{:,}".format(model.total_params)}')
 
-        # if neox_args.deepspeed:
-        #     print_rank_0("DeepSpeed is enabled.")
-        #     if neox_args.no_load_optim:
-        #         assert optimizer is None
-        #         _model_params = None
-        #         _lr_scheduler = None
-        #     else:
-        #         _model_params = param_groups if optimizer is None else None
-        #         _lr_scheduler = lr_scheduler
-        #
-        #     model, optimizer, _, lr_scheduler = deepspeed.initialize(
-        #         model=model,
-        #         optimizer=optimizer,
-        #         args=neox_args,
-        #         lr_scheduler=_lr_scheduler,
-        #         dist_init_required=False,
-        #         model_parameters=_model_params,
-        #         config_params=neox_args.deepspeed_config,
-        #         mpu=mpu if not neox_args.is_pipe_parallel else None,
-        #     )
-        #     model.total_params = get_total_params(model.module)
-        #     print_rank_0(f' > total params: {"{:,}".format(model.total_params)}')
-        #     #        raise ValueError("Stopping here so we can check!")  ## VAV DEBUG
-        #     if neox_args.is_pipe_parallel:
-        #         import pdb; pdb.set_trace()
-        #         model.set_has_attention_mask(True)
-        #         model.set_batch_fn(model.module._megatron_batch_fn)
-        # else:
-        #     raise ValueError("Must be using deepspeed to run neox")
+        if neox_args.deepspeed:
+            print_rank_0("DeepSpeed is enabled.")
+            if neox_args.no_load_optim:
+                assert optimizer is None
+                _model_params = None
+                _lr_scheduler = None
+            else:
+                _model_params = param_groups if optimizer is None else None
+                _lr_scheduler = lr_scheduler
 
-        # if neox_args.load is not None:
-        #     neox_args.iteration = load_checkpoint(neox_args, model,
-        #                                           optimizer=optimizer,
-        #                                           lr_scheduler=lr_scheduler,
-        #                                           inference=False, iteration=None)
-        #     print_rank_0(
-        #         f"Loading checkpoint and starting from iteration {neox_args.iteration}"
-        #     )
+            model, optimizer, _, lr_scheduler = deepspeed.initialize(
+                model=model,
+                optimizer=optimizer,
+                args=neox_args,
+                lr_scheduler=_lr_scheduler,
+                dist_init_required=False,
+                model_parameters=_model_params,
+                config_params=neox_args.deepspeed_config,
+                mpu=mpu if not neox_args.is_pipe_parallel else None,
+            )
+            model.total_params = get_total_params(model.module)
+            print_rank_0(f' > total params: {"{:,}".format(model.total_params)}')
+            #        raise ValueError("Stopping here so we can check!")  ## VAV DEBUG
+            if neox_args.is_pipe_parallel:
+                model.set_has_attention_mask(True)
+                model.set_batch_fn(partial(get_batch_pipe, neox_args=neox_args))
+        else:
+            raise ValueError("Must be using deepspeed to run neox")
+
+        if neox_args.load is not None:
+            neox_args.iteration = load_checkpoint(neox_args, model,
+                                                  optimizer=optimizer,
+                                                  lr_scheduler=lr_scheduler,
+                                                  inference=False, iteration=None)
+            print_rank_0(
+                f"Loading checkpoint and starting from iteration {neox_args.iteration}"
+            )
         # else:
         neox_args.iteration = 0
         return model, optimizer, lr_scheduler
@@ -120,19 +116,22 @@ def varmisuse_multi_class(neox_args, num_classes):
     """Finetune/evaluate."""
     finetune(neox_args, partial(model_optim_lr_setup, num_classes=num_classes),
              build_train_valid_test_data_iterators,
-             compute_varmisuse_loss)
+             compute_varmisuse_loss, evaluate)  # evaluate_varmisuse)
 
 
-def compute_varmisuse_loss(label_data, logits, metric=False):
+def compute_varmisuse_loss(logits, label_data, metric=False):
     # Unpack label data
     # (error_locations, repair_loc), (lengths, has_bug, repair_cands) = label_data
     # print("VAV DEBUG dp & mp rank:", mpu.get_data_parallel_rank(), mpu.get_model_parallel_rank())
     # print("VAV DEBUG devices:", error_locations.device, target_mask.device, mask.device, has_bug.device, logits.device)
 
+    # import pdb; pdb.set_trace()
     nbatch, seq_length, _ = logits.shape
 
+    # VAV: deepspeed code automatically moves the label data onto the correct device
     label_data = [l.to(logits.device) for l in label_data]
     error_locations, target_mask, mask, has_bug = label_data
+    # error_locations = error_locations.squeeze()
     n_buggy = torch.sum(has_bug).item()
     eps = 1e-16
 
@@ -145,8 +144,6 @@ def compute_varmisuse_loss(label_data, logits, metric=False):
                                        y_true=error_locations[~has_bug])
         loc_acc_bug = accuracy_score(y_pred=loc_max[has_bug],
                                      y_true=error_locations[has_bug])
-    else:
-        loc_acc_nobug, loc_acc_bug, mean_repair_acc, joint_acc = None, None, None, None
 
     # Repair loss is only computed at buggy samples (with error locations),
     # using (negative) cross-entropy
@@ -156,76 +153,135 @@ def compute_varmisuse_loss(label_data, logits, metric=False):
     ### trying to match hellendoorn metric
     pointer_logits = repair_pred + (1.0 - mask) * eps
     pointer_probs = torch.nn.functional.softmax(pointer_logits, dim=-1)
-    target_probs = (target_mask * pointer_probs).sum(-1)
-    repair_loss = -torch.log(target_probs[has_bug] + eps).sum() / (eps + n_buggy)
-    # Since this is a multi-label problem, we need a threshold for marking a location.
-    # For simplicity and to follow the previous work, we use a probability thresh of 0.5.
+    if n_buggy > 0:
+        target_probs = (target_mask * pointer_probs).sum(-1)
+        repair_loss = -torch.log(target_probs[has_bug] + eps).sum() / (eps + n_buggy)
+        loss = loc_loss + repair_loss
+    else:
+        # ignore repair loss element since there are no buggy samples in the batch
+        loss = loc_loss
     if metric:
+        # Since this is a multi-label problem, we need a threshold for marking a location.
+        # For simplicity and to follow the previous work, we use a probability thresh of 0.5.
         ### trying to match hellendoorn metric
         target_loc_acc = (target_probs[has_bug].detach().clone() > 0.5).type(torch.FloatTensor)
+        mean_repair_acc = target_loc_acc.mean()
         # Also compute the joint accuracy, but on buggy samples only
         lacc_by_sample = (loc_max[has_bug] == error_locations[has_bug]).type(torch.FloatTensor)
         joint_acc = (lacc_by_sample * target_loc_acc).mean()
-    loss = loc_loss + repair_loss
+
 
     # Reduce loss for logging.
     # averaged_loss = average_losses_across_data_parallel_group([loss])
 
     # return loss, {'lm loss': averaged_loss[0]}
-    return loss, (loc_acc_nobug, loc_acc_bug, mean_repair_acc, joint_acc)
+    if metric:
+        return loss, (loc_acc_nobug, loc_acc_bug, mean_repair_acc, joint_acc)
+    else:
+        return loss
 
 
-# def compute_varmisuse_loss_create_mask(label_data, logits, metric=False):
-#     # Unpack label data
-#     (error_locations, repair_loc), (lengths, has_bug, repair_cands) = label_data
-#
+def evaluate_varmisuse(neox_args, forward_step_fn, data_iterator, model, verbose=False, timers=None):
+    # VAV: I wanted this to be called during training, but it's too difficult to get the other
+    # metrics without altering deepspeed functions. So let's just do it after loading the model at
+    # certain checkpoints...
+    """Evaluation.
+    neox_args: NeoX Arguments
+    forward_step_fn: function with args `neox_args, timers,
+                    data_iterator & model that will run a forward pass on the model
+    data_iterator: Iterator that iterates over batches of data. Should return data in the form:
+                    {'text': np.array([tokens], dtype=np.int64)}
+                    where the size of the array is the model's context size + 1
+                    (`get_batch` transforms it into inputs / labels)
+    """
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    losses = []
+    metric_lists = [[], [], [], []]
+    metric_labels = ['loc_acc_nobug', 'loc_acc_bug', 'repair_acc', 'joint_acc']
+
+    with torch.no_grad():
+        iteration = 0
+        while iteration < neox_args.eval_iters:
+            iteration += 1
+            if verbose and iteration % neox_args.log_interval == 0:
+                print_rank_0(
+                    "Evaluating iter {}/{}".format(iteration, neox_args.eval_iters)
+                )
+
+            # although we're not accumulating gradients here, we count one iter as train_batch_size_per_gpu * g.a.s
+            # to be consistent with deepspeed's pipe parallel engine
+            # since pipe parallel already takes gas into account - default to 1 here if pipe parallel is true
+            for _ in range(
+                1
+                if neox_args.is_pipe_parallel
+                else neox_args.gradient_accumulation_steps
+            ):
+                # Forward evaluation
+                loss, metric_info = forward_step_fn(
+                    model=model,
+                    data_iterator=data_iterator,
+                    neox_args=neox_args,
+                    timers=timers,
+                )
+                for i, metric in enumerate(metric_info):
+                    metric_lists[i].append(metric)
+                losses.append(loss)
+
+            # When contiguous memory optimizations are enabled, the buffers
+            # allocated by the optimizations are deallocated during backward pass
+            # in the absence of backward pass the buffers should be reset after each
+            # forward pass
+            if neox_args.deepspeed and neox_args.deepspeed_activation_checkpointing:
+                deepspeed.checkpointing.reset()
+
+    # reduces losses across processes for logging & run eval harness tasks
+    eval_results = {"lm_loss": reduce_losses(losses).mean().item()}
+    eval_results["lm_loss_ppl"] = math.exp(eval_results["lm_loss"])
+    for i, k in enumerate(metric_labels):
+        eval_results[k] = reduce_losses(metric_lists[i]).mean().item()
+
+    # Move model back to the train mode.
+    model.train()
+    return eval_results
+
+
+# def compute_varmisuse_acc(logits, label_data):
 #     nbatch, seq_length, _ = logits.shape
-#     n_buggy = torch.sum(has_bug).item()
+#
+#     # Unpack label data
+#     error_locations, target_mask, mask, has_bug = label_data
+#     # n_buggy = torch.sum(has_bug).item()
 #     eps = 1e-16
-#     # Create sequence length mask. Mask values of 1 are kept. Default 0.
-#     mask = torch.zeros((nbatch, seq_length), dtype=torch.long, device=logits.device)
-#     for b in range(nbatch):
-#         mask[b, repair_cands[b]] = 1.
 #
 #     # Localization loss is simply calculated with sparse CE
-#     loc_pred = logits[:, :, 0]  # * mask  # (nbatch, seq_length)
-#     loc_loss = torch.nn.functional.cross_entropy(loc_pred, error_locations)
-#     if metric:
-#         loc_max = loc_pred.detach().clone().softmax(dim=-1).argmax(dim=-1)
-#         loc_acc_nobug = accuracy_score(y_pred=loc_max[~has_bug],
-#                                        y_true=error_locations[~has_bug])
-#         loc_acc_bug = accuracy_score(y_pred=loc_max[has_bug],
-#                                      y_true=error_locations[has_bug])
-#     else:
-#         loc_acc_nobug, loc_acc_bug, mean_repair_acc, joint_acc = None, None, None, None
+#     loc_pred = logits[:, :, 0]
+#     # loc_loss = torch.nn.functional.cross_entropy(loc_pred, error_locations)
+#     loc_max = loc_pred.detach().clone().softmax(dim=-1).argmax(dim=-1)
+#     loc_acc_nobug = accuracy_score(y_pred=loc_max[~has_bug],
+#                                    y_true=error_locations[~has_bug])
+#     loc_acc_bug = accuracy_score(y_pred=loc_max[has_bug],
+#                                  y_true=error_locations[has_bug])
 #
 #     # Repair loss is only computed at buggy samples (with error locations),
 #     # using (negative) cross-entropy
 #     repair_pred = logits[:, :, 1] * mask  # (nbatch, seq_length)
 #     # repair_locations becomes a (nbatch, seq_length) Tensor of mostly 0s
 #     #  it has values of 1 if it is a repair location. each row can have >1 label
-#     target_mask = torch.vstack(
-#         [torch.nn.functional.one_hot(rlab, num_classes=seq_length).sum(dim=0).to(rlab.device) for ii, rlab in enumerate(repair_loc)])
 #     ### trying to match hellendoorn metric
 #     pointer_logits = repair_pred + (1.0 - mask) * eps
 #     pointer_probs = torch.nn.functional.softmax(pointer_logits, dim=-1)
 #     target_probs = (target_mask * pointer_probs).sum(-1)
-#     repair_loss = -torch.log(target_probs[has_bug] + eps).sum() / (eps + n_buggy)
 #     # Since this is a multi-label problem, we need a threshold for marking a location.
 #     # For simplicity and to follow the previous work, we use a probability thresh of 0.5.
-#     if metric:
-#         ### trying to match hellendoorn metric
-#         target_loc_acc = (target_probs[has_bug].detach().clone() > 0.5).type(torch.FloatTensor)
-#         # Also compute the joint accuracy, but on buggy samples only
-#         lacc_by_sample = (loc_max[has_bug] == error_locations[has_bug]).type(torch.FloatTensor)
-#         joint_acc = (lacc_by_sample * target_loc_acc).mean()
-#     loss = loc_loss + repair_loss
+#     ### trying to match hellendoorn metric
+#     target_loc_acc = (target_probs[has_bug].detach().clone() > 0.5).type(torch.FloatTensor)
+#     mean_repair_acc = target_loc_acc.mean()
+#     # Also compute the joint accuracy, but on buggy samples only
+#     lacc_by_sample = (loc_max[has_bug] == error_locations[has_bug]).type(torch.FloatTensor)
+#     joint_acc = (lacc_by_sample * target_loc_acc).mean()
 #
-#     # Reduce loss for logging.
-#     averaged_loss = average_losses_across_data_parallel_group([loss])
-#
-#     # return loss, {'lm loss': averaged_loss[0]}
-#     return loss, (loc_acc_nobug, loc_acc_bug, mean_repair_acc, joint_acc)
+#     return loc_acc_nobug, loc_acc_bug, mean_repair_acc, joint_acc
 
 
 if __name__ == "__main__":

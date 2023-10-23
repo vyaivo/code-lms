66e3b0cb85a17fe662b39c1439d652dce0bd1f49
Vy Vo <vy.vo@intel.com>
2023-10-23 11:18:17 -0700

    added script for inference/generation

---
+++ polycoder/tasks/omp/omp_test_generation.py
@@ -0,0 +1,40 @@
+import datasets as trd
+import os, sys
+import pickle
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from megatron.text_generation_utils import save_generated_samples_from_list
+from megatron.utils import setup_for_inference_or_eval
+
+
+run_generation = True
+analyze = True
+
+# Setup / load the model
+model, neox_args = setup_for_inference_or_eval(inference=True, get_key_value=True, overwrite_values=None)
+
+# Load the test dataset
+dataset_path = neox_args.finetune_data_path
+split = "test"
+print(f"Loading dataset from {dataset_path}{split}...")
+d = trd.load_from_disk(os.path.join(dataset_path, split))
+samples = d['code']
+labels = d['pragma']
+
+if run_generation:
+    # Now run the generation!
+    print(f"Now running the prompts through model for generation...")
+    save_generated_samples_from_list(neox_args, model, samples)
+
+if analyze:
+    print(f"Writing out the generated outputs vs labels...")
+    with open(f'{os.getcwd()}/generated_output.pkl', 'rb') as f:
+        gen_data = pickle.load(f)
+    # Write output to text file for now
+    output_path = "omp_generation_outputs.txt"
+    data_path = os.path.join(dataset_path, output_path)
+    with open(os.path.join(dataset_path, output_path), 'wt') as outf:
+        for gen, label in zip(gen_data, labels):
+            outf.write(f"{label}\n{gen['text']}\n\n\n")
+    print(f"Written to {data_path}!")
+
